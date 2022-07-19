e448ba7783d418b0b95c72bece0d2c347d7c8631
Kumitasu <grzybolym@gmail.com>
2022-07-19 16:55:29 +0200

    Header jsx

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,9 +9,11 @@ const Header = () => (
     <div className='app__wrapper-info'>
       <SubHeading title='Chase the new flavour'/>
       <h1 className='app__header-h1'>The key to Fine Dining</h1>
+      <p className='p__opensans' style={{margin: '2rem 0'}}>lorem ipsum dolor sit amet, consectetur</p>
+      <button type='button' className='custom__button'>Explore Menu</button>
     </div>
-    <div className='app__wrapper_image'>
-
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt='header img' />
     </div>
   </div>
 );
