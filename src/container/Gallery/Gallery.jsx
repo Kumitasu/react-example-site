4ffbe97cda7d721a4a9befe5264796a5b7ffed7d
Kumitasu <grzybolym@gmail.com>
2022-08-17 15:35:42 +0200

    jsx

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -5,11 +5,19 @@ import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import './Gallery.css';
 
+const galleryImages = [images.gallery01, images.gallery02, images.gallery03, images.gallery04 ];
+
 const Gallery = () => {
   const scrollRef = React.useRef(null);
 
   const scroll = (direction) => {
     const { current } = scrollRef;
+
+    if(direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
   }
 
   return (
@@ -23,7 +31,11 @@ const Gallery = () => {
     
     <div className='app__gallery-images'>
       <div className='app__gallery-images_container' ref={scrollRef}>
-
+        {galleryImages.map((image, index) => (
+          <div className='app__gallery-images_card flex__center' key={`gallery_image-${index + 1}`}>
+            <img src={image} alt='gallery'/>
+          </div>
+        ))}
       </div>
       <div className='app__gallery-images_arrow'>
         <BsArrowLeftShort className='galler__arrow-icon' onClick={() => scroll('left')} />
