3a305a4aeb9e4aa7f9973f962f3bc144af50a09a
Kumitasu <grzybolym@gmail.com>
2022-09-23 14:36:20 +0200

    jsx

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -4,16 +4,15 @@ import SubHeading from '../SubHeading/SubHeading';
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div className='app__newsletter'>
-    <div className='app__newsletter-heading'>
-      <SubHeading title='Newsletter' />
-      <h1 className='headtext__cormorant'>Subscribe</h1>
-      <p className='p__opensans'>lorem ipsum</p>
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
+      <p className="p__opensans">And never miss latest Updates!</p>
     </div>
-
-    <div className='app__newsletter-input flex__center'>
-      <input type='email' placeholder='Enter your email address' />
-      <button className='custom__button'>Submit</button>
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder="Enter your email address" />
+      <button type="button" className="custom__button">Subscribe</button>
     </div>
   </div>
 );
