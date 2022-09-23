b39121a4323980211eef53ace38d79126f397ca6
Kumitasu <grzybolym@gmail.com>
2022-09-23 14:27:08 +0200

    jsx

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -8,6 +8,22 @@ const Footer = () => (
   <div className='app__footer section__padding'>
     <FooterOverlay />
     <Newsletter />
+
+    <div className='app__footer-links'>
+      <div className='app__footer-links_contact'>
+        Hello
+      </div>
+      <div className='app__footer-links_logo'>
+
+      </div>
+      <div className='app__footer-links_work'>
+
+      </div>
+    </div>
+
+    <div className='footer__copyright'>
+      <p className='p__opensans'>Gericht</p>
+    </div>
   </div>
 );
 
